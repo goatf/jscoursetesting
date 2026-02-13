c2904365e893ed4063c4b774ea29f02209e49ec5
Jirawat Praposri <jirawatgreen@gmail.com>
2026-02-13 10:19:37 +0700

    Practice task

--- control_flow.js
+++ control_flow.js
@@ -45,3 +45,18 @@ console.log("User Category:", userCategory);
 let isAuthenticated = true;
 let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 console.log("Authentication Status:", authenticationStatus);
+
+/*Practice task*/
+if (userRole === "Employee") {
+    accessLevel = "Access to Dietary Services";
+} else if (userRole === "Enrolled Member") {
+    accessLevel = "Access to Dietary Services and one-on-one interaction with a dietician";
+} else if(userRole === "Subscriber"){
+    accessLevel = " Access to facilitate Dietary Services only";
+}
+else
+{
+    accessLevel = "Need to enroll or at least subscribe first to avail this facility";
+}
+
+console.log("Access Level:", accessLevel);
